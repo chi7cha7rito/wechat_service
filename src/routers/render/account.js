f7b1a5b4f485225d59794a1e133b4e3f02a758f6
Kingsley <Yu@KingsleydeMacBook-Pro.local>
2017-03-12 23:16:31 +0800

    update

--- src/routers/render/account.js
+++ src/routers/render/account.js
@@ -5,5 +5,18 @@ import routerUtil from "../../utils/router";
 
 let router = express.Router();
 
+router.get('/recharge', (req, res, next) => {
+  let param = {
+    req: req,
+    matchJavascript: true,
+    matchStylesheet: true
+  }
+
+  let templateData = routerUtil.getTemplateBasicData(param);
+
+  Object.assign(templateData, { "title": "在线充值" });
+
+  return res.render("account/recharge", templateData);
+})
 
 module.exports = router
--- src/routers/render/personal.js
+++ src/routers/render/personal.js
@@ -33,4 +33,34 @@ router.get('/consume', (req, res, next) => {
   return res.render("personal/consume", templateData);
 })
 
+router.get('/info', (req, res, next) => {
+  let param = {
+    req: req,
+    matchJavascript: true,
+    matchStylesheet: true
+  }
+
+  let templateData = routerUtil.getTemplateBasicData(param);
+
+  Object.assign(templateData, { "title": "修改个人信息" });
+
+  return res.render("personal/info", templateData);
+})
+
+router.get('/recharge', (req, res, next) => {
+  let param = {
+    req: req,
+    matchJavascript: true,
+    matchStylesheet: true
+  }
+
+  let templateData = routerUtil.getTemplateBasicData(param);
+
+  Object.assign(templateData, { "title": "充值记录" });
+
+  return res.render("personal/recharge", templateData);
+})
+
+
+
 module.exports = router
