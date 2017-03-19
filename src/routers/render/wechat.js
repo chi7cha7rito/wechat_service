d72030b31b69e5eadd1f237f441a9ea92da71531
Kingsley <Yu@172-13-6-198.lightspeed.mssnks.sbcglobal.net>
2017-03-19 14:06:43 +0800

    update wechat pay

--- src/routers/render/wechat.js
+++ src/routers/render/wechat.js
@@ -3,14 +3,28 @@ import wechat from 'wechat'
 import OAuth from 'wechat-oauth'
 import logger from '../../utils/logger';
 import config from '../../utils/config';
+import Payment from 'wechat-pay';
 
 
 let router = express.Router()
 let wechatConfig = config.getWechat();
 
+//auth client
 let client = new OAuth(wechatConfig.appid, wechatConfig.secret);
 
+//wechat pay unifiedorder 基本参数
+let payment = new Payment({
+  "appId": wechatConfig.appid,
+  "mchId": wechatConfig.mch_id
+})
 
+//wechat pay notify middleware
+let middleware = Payment.middleware;
+
+
+/**
+ * desc:微信验证Token的url
+ */
 router.all('/', wechat(wechatConfig.token, async (req, res, next) => {
   var message = req.weixin
   let open_id = message.FromUserName
@@ -164,5 +178,98 @@ router.all('/auth', async (req, res, next) => {
     }
   });
 })
+
+/**
+ * desc:微信支付调用统一下单接口，获取prepay_id
+ * @see https://api.mch.weixin.qq.com/pay/unifiedorder
+ */
+router.get('/getPrePayInfo', (req, res, next) => {
+  try {
+    let body = req.body.name;
+    let attach = "";
+    let total_fee = req.body.price * 100;
+    let spbill_create_ip = req.ip;
+    let openid = req.session.member.wechat.open_id;
+    let out_trade_no='hulk_club' +(new Date().valueOf());
+    let trade_type = "JSAPI";
+
+    let order = {
+      body,
+      attach,
+      total_fee,
+      spbill_create_ip,
+      out_trade_no,
+      openid,
+      trade_type
+    }
+
+    let respData={
+      "status":"1",
+      "message":"",
+      "data":null
+    }
+
+    payment.getBrandWCPayRequestParams(order, function (err, payargs) {
+      respData.data=payargs;
+      res.json(respData);
+    });
+
+  } catch (e) {
+    logger.error("wechat_getPrePayInfo_error:" + JSON.stringify(e));
+    respData.status="0";
+    respData.message="获取统一下单信息失败";
+    resp.data=null;
+    res.json(respData);
+  }
+})
+
+/**
+ * desc:接收微信付款确认请求
+ * @see https://api.mch.weixin.qq.com/pay/unifiedorder
+ */
+router.all('/payNotify', middleware(initConfig).getNotify().done(function (message, req, res, next) {
+  var openid = message.openid;
+  var order_id = message.out_trade_no;
+  var attach = {};
+  try {
+    attach = JSON.parse(message.attach);
+  } catch (e) { }
+
+  /**
+   * 查询订单，在自己系统里把订单标为已处理
+   * 如果订单之前已经处理过了直接返回成功
+   */
+  res.reply('success');
+
+  /**
+   * 有错误返回错误，不然微信会在一段时间里以一定频次请求你
+   * res.reply(new Error('...'))
+   */
+}));
+
+
+/**
+ * desc:微信付款页面，由于公众号支付页面需要支付的授权目录下面
+ */
+router.get("/pay",(req,res,next)=>{
+  try{
+    let param = {
+    req: req,
+    matchJavascript: true,
+    matchStylesheet: true
+  }
+
+  let templateData = routerUtil.getTemplateBasicData(param);
+
+  Object.assign(templateData, { "title": "在线充值" });
+
+  return res.render("wechat/pay", templateData);
+    }
+    catch(e){
+      logger.error("wechat_pay_page_error"+JSON.stringify(e));
+    }
+  
+})
+
 
 module.exports = router
